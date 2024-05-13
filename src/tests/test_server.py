c44eb4d6d110d30d033ec1388279964dd6c75e1f
CosimoSFischer <165454022+CosimoSFischer@users.noreply.github.com>
2024-05-13 15:13:15 +0200

    Update test_server.py

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -3,7 +3,7 @@ from server.server import deserialise_data
 
 class TestServer(unittest.TestCase):
     
-    def test_deserialize_data(self):
+    def test_deserialise_data(self):
         data = '{"name": "Alice", "age": 25, "is_active": true}'
         deserialized = deserialise_data(data.encode('utf-8'), 'json')
         self.assertEqual(deserialised['name'], 'Alice')
